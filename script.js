95ee8fa2e69180eee7e51a09e07cb82ced6deafe
Drake Strike <phjtieudoc@users.noreply.github.com>
2020-02-07 18:54:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -30,6 +30,11 @@ $(function()
     name: "Tháng Tư Là Lời Nói Dối Của Em",
     url: "https://docs.google.com/uc?export=download&id=1D8e6YTLNHDEooxq_VbxP3vScyOcm8QHm",
     picture: "https://avatar-nct.nixcdn.com/song/2017/09/22/1/a/3/6/1506063956390_640.jpg"
+}, {
+    artist: "Tino",
+    name: "Ừ Có Anh Đây",
+    url: "https://docs.google.com/uc?export=download&id=1emYz8dFA0a0AwtVZGiMqk9_LV9USvEC8",
+    picture: "https://photo-zmp3.zadn.vn/covers/0/4/047305f465f0aa64c22ed49787b02548_1510199251.jpg"
 }];
 	
 	function shuffle(a) {
